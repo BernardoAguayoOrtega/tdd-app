e3fcf33eae5837d060a154c819055143d56e25bf
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-22 18:43:00 -0500

    Test heading

---
+++ src/components/GithubSearch/GithubSearch.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+
+import {GithubSearch} from './index'
+
+describe('when the GithubSearch component is mounted', () => {
+  it('should display the title', () => {
+    const {getByRole} = render(<GithubSearch />)
+    getByRole('heading', {name: /github repositories list/i})
+  })
+})
---
+++ src/components/GithubSearch/index.js
@@ -0,0 +1,1 @@
+export const GithubSearch = () => <h1>Github repositories list</h1>
