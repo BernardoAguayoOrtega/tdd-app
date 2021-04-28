4020a7435ef7382d5381dd1b814946c9202bf950
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-27 21:42:13 -0500

    add styles

--- src/components/GithubSearch/index.js
+++ src/components/GithubSearch/index.js
@@ -1,11 +1,17 @@
-import {Typography, TextField, Button} from '@material-ui/core'
+import {Typography, TextField, Button, Container, Grid} from '@material-ui/core'
 
 export const GithubSearch = () => (
-  <>
+  <Container>
     <Typography variant="h3" component="h1">
       Github repositories list
     </Typography>
-    <TextField label="filter by" id="filter by" />
-    <Button>Search</Button>
-  </>
+    <Grid container spacing={2}>
+      <Grid item md={6} xs={12}>
+        <TextField fullWidth label="filter by" id="filter by" />
+      </Grid>
+      <Grid item md={3} xs={12}>
+        <Button fullWidth color="primary" variant="contained">Search</Button>
+      </Grid>
+    </Grid>
+  </Container>
 )
