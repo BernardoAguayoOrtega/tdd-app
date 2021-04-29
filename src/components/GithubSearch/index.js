1990c46764ffa9c81d554158c576a11223ad2b0c
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2021-04-28 20:54:22 -0500

    add tests to check if the button will be disable after a clicl

--- src/components/GithubSearch/index.js
+++ src/components/GithubSearch/index.js
@@ -6,31 +6,48 @@ import {
   Grid,
   Box,
 } from '@material-ui/core'
+import {useState} from 'react'
 
-export const GithubSearch = () => (
-  <Container>
-    <Typography variant="h3" component="h1">
-      Github repositories list
-    </Typography>
-    <Grid container spacing={2}>
-      <Grid item md={6} xs={12}>
-        <TextField fullWidth label="filter by" id="filter by" />
-      </Grid>
-      <Grid item md={3} xs={12}>
-        <Button fullWidth color="primary" variant="contained">
-          Search
-        </Button>
-      </Grid>
-    </Grid>
-    <Box
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      height="100"
-    >
-      <Typography>
-        Please provide a search option and click in the search button
+export const GithubSearch = () => {
+  const [isSearching, setIsSearching] = useState(false)
+
+  const handleClick = event => {
+    console.log('click')
+    event.preventDefault()
+    setIsSearching(true)
+  }
+
+  return (
+    <Container>
+      <Typography variant="h3" component="h1">
+        Github repositories list
       </Typography>
-    </Box>
-  </Container>
-)
+      <Grid container spacing={2}>
+        <Grid item md={6} xs={12}>
+          <TextField fullWidth label="filter by" id="filter by" />
+        </Grid>
+        <Grid item md={3} xs={12}>
+          <Button
+            onClick={handleClick}
+            disabled={isSearching}
+            fullWidth
+            color="primary"
+            variant="contained"
+          >
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        height="100"
+      >
+        <Typography>
+          Please provide a search option and click in the search button
+        </Typography>
+      </Box>
+    </Container>
+  )
+}
